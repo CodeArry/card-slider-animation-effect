e7d8c9a3bda3c8c61d1990cd54064c4ae0bcf07b
CodeArry <codewitharry2004@gmail.com>
2024-08-01 23:07:41 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+  
+  slidesPerView: 3,
+  centeredSlides: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+
+    dynamicBullets: true,
+    clickable: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+// Triggers to update the main background
+// when doing the next slide
+swiper.on('slideNextTransitionStart', () => {
+    updateMainBackground()
+})
+
+// Triggers to update the main background
+// when doing the previous slide
+swiper.on('slidePrevTransitionStart', () => {
+    updateMainBackground()
+})
+
+function updateMainBackground() {
+    // Get the currently active slide element
+    const activeSlide = document.querySelector('.swiper-slide-active')
+    // Get the main container element
+    const mainContainer = document.querySelector('.main-slides')
+    // Update the background style of the main container
+    // to match the active slide
+    mainContainer.style.background = activeSlide.style.background
+}
